e34f5dfd9b3b4dbaaa43bf232f846a120c3cd88f
fengwenyi <xfsy_2015@163.com>
2018-04-02 13:30:47 +0800

    Initial commit

---
+++ toolbar/src/main/java/com/fengwenyi/app/toolbar/day_20180402/TestActivity.java
@@ -0,0 +1,33 @@
+package com.fengwenyi.app.toolbar.day_20180402;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.fengwenyi.app.toolbar.R;
+
+/**
+ * @author Wenyi Feng
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tool_test);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
